263cd14a2005fa7065d52e87409549db353a29e0
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-01-21 10:48:43 +0530

    rectoring of code

--- Task-2/Calculator/index.js
+++ Task-2/Calculator/index.js
@@ -1,10 +1,9 @@
 let output=document.getElementById('output-value');
-buttons=document.querySelectorAll('button');
+let buttons=document.querySelectorAll('button');
 let history=document.getElementById('history-value');
 let calculate=[];
-let norepeatofOperator=[];
 for(bt of buttons){
-    bt.addEventListener('click',(e)=>{
+        bt.addEventListener('click',(e)=>{
         buttonText=e.target.innerText;
         history.innerHTML+=buttonText;
         if(buttonText=='C'){
@@ -22,41 +21,30 @@ for(bt of buttons){
                     calculate[v]=calculate[v]+calculate[v+1];
                     calculate.pop(calculate[v+1]);
                 }
-                    if(calculate[v]==calculate[v+1] && ['+','-','x','/','%'].includes(calculate[v])){
-                    calculate.pop(calculate[v+1]);
-                    temp=history.innerHTML;
-                    history.innerHTML=temp.substring(0,temp.length-1);
+                if(calculate[v]==calculate[v+1] && ['+','-','x','/','%'].includes(calculate[v])){
+                calculate.pop(calculate[v+1]);
+                temp=history.innerHTML;
+                history.innerHTML=temp.substring(0,temp.length-1);
                 }
             }
-            output.innerHTML=buttonText;
-            console.log('1',calculate);
-            
+            output.innerHTML=buttonText;    
         }
     })
 
 }
 
-function calci(){
+let calci=()=>{
     if(calculate.length==3){
         let result;
         [a, operatorString, b] = calculate;
         a=parseInt(a);
         b=parseInt(b);
         switch(operatorString) { 
-                case '+': 
-                      result=a+b;
-                      break; 
-                case '-': 
-                      result=a-b; 
-                      break;
-                case 'x': 
-                      result=a*b; 
-                      break;
-                case '/':
-                      result=a/b; 
-                      break;
-                case '%':
-                      result=a%b; 
+                case '+': result=a+b;break; 
+                case '-': result=a-b; break;
+                case 'x': result=a*b; break;
+                case '/': result=a/b; break;
+                case '%': result=a%b; 
             
         } 
         while(calculate.length > 0) {
