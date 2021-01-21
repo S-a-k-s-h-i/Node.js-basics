6ae48acf0761f16bfa4c9c097c2b31a9afe5f4e3
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-01-21 16:33:13 +0530

    chat app

--- Task-3/chat App/index.js
+++ Task-3/chat App/index.js
@@ -11,15 +11,11 @@ let chats=document.getElementById("chats");
 button1.addEventListener('click',function(){
    allMessages.push(textbox1.value);
    u1=user1.options[user1.selectedIndex].text;
-   console.log(allMessages);
-   console.log('u1',u1);
    textbox1.value="";
 })
 button2.addEventListener('click',function(){
     allMessages.push(textbox2.value);
     u2=user2.options[user2.selectedIndex].text;
-    console.log(allMessages);
-    console.log('u2',u2);
     textbox2.value="";
    })
 
@@ -36,21 +32,18 @@ chats.addEventListener('click',function(){
              user1Message.appendChild(span);
              messages.appendChild(user1Message);
        }else{
-        var user2Message=document.createElement("div");
-        user2Message.classList.add("msg2");
-        user2Message.innerHTML=allMessages[m];
-        span=document.createElement('span');
-        span.classList.add("sender");
-        sender=document.createTextNode(u2);
-        span.appendChild(sender);
-        user2Message.appendChild(span);
-        messages.appendChild(user2Message)
+             var user2Message=document.createElement("div");
+             user2Message.classList.add("msg2");
+             user2Message.innerHTML=allMessages[m];
+             span=document.createElement('span');
+             span.classList.add("sender");
+             sender=document.createTextNode(u2);
+             span.appendChild(sender);
+             user2Message.appendChild(span);
+             messages.appendChild(user2Message)
        }
-   }
+    }
    allMessages=[];
 }
-)
-
-
-  ;
+);
 
