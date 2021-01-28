5c6e5136175a1c1e80649d569d1f7784793303e1
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-01-28 18:31:27 +0530

    middleware

--- expressJs/middleware.js
+++ expressJs/middleware.js
@@ -3,27 +3,41 @@ const app=express();
 const port=8080
 
 // MIDDLEWARE function mylogger
-// var mylogger=function(req,res,next){
-//     console.log("LOGGED");
-//     next();
-// }
-// app.use(mylogger);
-// app.get('/',(req,res) => {
-//     res.send('Hello Middleware!!!');
-// })
+app.use(mylogger);
+function mylogger(req,res,next){
+    console.log("LOGGED");
+    console.log(req.originalUrl);
+    next();
+}
+function auth(req,res,next){
+    if(req.query.admin==='true'){
+        req.admin=true;
+        next()
+    }else{
+        res.send('No Auth');
+    }
+}
+
+app.get('/',(req,res) => {
+    res.send('Hello Middleware!!!');
+})
+app.get('/users',auth,(req,res) => {
+    console.log(`user admin is ${req.admin}`);
+    res.send("User Page");
+})
 
 
 // MIDDLEWARE function requestTime
-var requestTime=function(req,res,next){
-    req.requestTime=Date.now();
-    next();
-}
-app.use(requestTime);
-app.get('/',(req,res) => {
-    var responseText="Hello World!!!!<br>";
-    responseText+="<small>Requested at"+req.requestTime+"</small>";
-    res.send(responseText);
-})
+// var requestTime=function(req,res,next){
+//     req.requestTime=Date.now();
+//     next();
+// }
+// app.use(requestTime);
+// app.get('/',(req,res) => {
+//     var responseText="Hello World!!!!<br>";
+//     responseText+="<small>Requested at"+req.requestTime+"</small>";
+//     res.send(responseText);
+// })
 
 app.listen(port,() => {
     console.log(`litening on http://localhost:${port}`);
