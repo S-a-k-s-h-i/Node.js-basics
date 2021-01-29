8095f2fb9171c864a27679913c7b313f4ed3c19e
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-01-29 19:44:49 +0530

    modules

--- nodejs/module/app.js
+++ nodejs/module/app.js
@@ -1,7 +1,12 @@
+const EventEmitter=require("events");
 var name="sakshi";
 
-function log(message){
-    console.log(name+message);
+class Emitter extends EventEmitter{
+    log(message){
+        console.log(name+message);
+        // Raising an event
+        this.emit("messageLogged",{id:1,url:"http://"})
+    }
 }
 // module.exports.log=log;
-module.exports=log;
+module.exports=Emitter;
