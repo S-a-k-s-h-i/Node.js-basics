d43e73536c537e19baecfb6546a8ac05c0c6f1c8
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-01-29 19:47:35 +0530

    process.env.port

--- expressJs/index.js
+++ expressJs/index.js
@@ -1,12 +1,18 @@
 const express=require("express");
 const app=express();
 const path=require("path");
-const port=8080;
+const port=process.env.PORT || 8000;
 const staticPath=path.join(__dirname,'/public');
 app.use(express.static(staticPath));
 
 app.get('/',(req,res) => {
-res.send()
+res.send("Welcome")
+})
+app.get('/api/courses/:id',(req,res) => {
+    res.send(req.params.id);
+})
+app.get('/api/posts/:year/:month',(req,res) => {
+    res.send(req.query);
 })
 app.listen(port,() => {
     console.log(`listening on http://localhost:${port}`);
